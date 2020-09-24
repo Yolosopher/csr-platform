068904240aa669c12dc59651248070ae50ea3bcd
Nika Nishnianidze <61516311+Yolosopher@users.noreply.github.com>
2020-09-24 08:00:35 +0400

    a

---
+++ assets/js/ps-gallery.js
@@ -0,0 +1,10 @@
+const spGallerySwiper = new Swiper(".personal-stories-inner", {
+    navigation: {
+        nextEl: ".main-slider-hc-next",
+        prevEl: ".main-slider-hc-prev",
+    },
+    pagination: {
+        el: ".main-slider-hc-pagination",
+    },
+    keyboard: true,
+});
